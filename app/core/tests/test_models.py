6258126a99cfef2a084c4bcd6085801c749fdcd5
kacper.pichola <kacper.pichola@amrest.eu>
2022-10-25 19:47:17 +0200

    Added user model functions + tests

--- app/core/tests/test_models.py
+++ app/core/tests/test_models.py
@@ -32,3 +32,18 @@ class ModelTests(TestCase):
         for email, expected in sample_emails:
             user = get_user_model().objects.create_user(email, 'SamplePWD123!;')
             self.assertEqual(user.email, expected)
+
+    def test_new_user_without_email_raises_error(self):
+        """Test if creating new user without an email raises error"""
+        with self.assertRaises(ValueError):
+            get_user_model().objects.create_user("", "SamplePWD123!;")
+        
+    def test_create_superuser(self):
+        """Test creating a super user"""
+        user = get_user_model().objects.create_superuser(
+            'test@example.com',
+            'SamplePWD123!;',
+        )
+
+        self.assertTrue(user.is_superuser)
+        self.assertTrue(user.is_staff)
